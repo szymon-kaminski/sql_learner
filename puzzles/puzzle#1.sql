382691b75d73ca9f0e266adb3f4d218483ba3d16
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-08-19 13:23:03 +0200

    create base table for shopping carts puzzle

---
+++ puzzles/puzzle#1.sql
@@ -0,0 +1,16 @@
+-- Puzzle #1: Shopping Carts
+
+-- Step 1: create table and insert sample data
+CREATE TABLE ShoppingCarts (
+    Cart1 VARCHAR(50),
+    Cart2 VARCHAR(50)
+);
+
+INSERT INFO ShoppingCarts (Cart1, Cart2) VALUES
+('SUGAR', 'SUGAR'),
+('BREAD', 'BREAD'),
+('JUICE', 'BUTTER'),
+('SODA', 'CHEESE'),
+('FLOUR', 'FRUIT');
+
+
