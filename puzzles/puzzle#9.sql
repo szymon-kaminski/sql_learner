ab1cf0b7cbd2ec49250288544e6108a7d5f1834c
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-08-26 14:17:12 +0200

    insert sample data

--- puzzles/puzzle#9.sql
+++ puzzles/puzzle#9.sql
@@ -12,3 +12,24 @@ CREATE TABLE EmployeeLicenses (
     EmployeeID INT,
     License VARCHAR(50)
 );
+
+-- Step 2 — Wstawienie danych
+
+INSERT INTO EmployeeLicenses (EmployeeID, License) VALUES
+(1001, 'Class A'),
+(1001, 'Class B'),
+(1001, 'Class C'),
+(2002, 'Class A'),
+(2002, 'Class B'),
+(2002, 'Class C'),
+(3003, 'Class A'),
+(3003, 'Class D'),
+(4004, 'Class A'),
+(4004, 'Class B'),
+(4004, 'Class D'),
+(5005, 'Class A'),
+(5005, 'Class B'),
+(5005, 'Class D');
+
+-- Podgląd tabeli
+SELECT * FROM EmployeeLicenses;
