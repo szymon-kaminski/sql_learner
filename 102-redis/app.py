5e00c6483d2877d4b05fcc3eb42509d8c264f37e
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-08-28 10:11:05 +0200

    Add user creation functionality

--- 102-redis/app.py
+++ 102-redis/app.py
@@ -7,6 +7,32 @@ def connect_redis():
         decode_responses=True
     )
 
+
+def add_user(r):
+    user_id = input("Podaj ID użytkownika: ")
+    name = input("Podaj imię i nazwisko: ")
+    email = input("Podaj email: ")
+    # Redis Hash
+    r.hset(f"user:{user_id}", mapping={"name": name, "email": email})
+    print("Użytkownik dodany!")
+
+
+def main():
+    r = connect_redis()
+    print("Połączono z Redisem!")
+
+    while True:
+        print("\n--- MENU ---")
+        print("1. Dodaj użytkownika")
+
+        choice = input("Wybierz opcję: ")
+
+        if choice == "1":
+            add_user(r)
+        else:
+            print("Nieprawidłowa opcja.")
+
+
 if __name__ == "__main__":
     r = connect_redis()
     print("Ping:", r.ping())
