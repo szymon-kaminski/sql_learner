363ce0aafc00ce5f941093c9787296e1b2415e04
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-09-02 13:04:02 +0200

    Add SQL syntax table and sample data

---
+++ puzzles/puzzle#15.sql
@@ -0,0 +1,25 @@
+-- Puzzle #15
+
+
+
+-- Step 1 — Create table and insert data
+
+USE sql_learner;
+
+DROP TABLE IF EXISTS SqlSyntax;
+CREATE TABLE SqlSyntax (
+    Sequence INT,
+    Syntax VARCHAR(100)
+);
+
+INSERT INTO SqlSyntax (Sequence, Syntax)
+VALUES
+(1, 'SELECT'),
+(2, 'Product,'),
+(3, 'UnitPrice,'),
+(4, 'EffectiveDate'),
+(5, 'FROM'),
+(6, 'Products'),
+(7, 'WHERE'),
+(8, 'UnitPrice'),
+(9, '> 100');
