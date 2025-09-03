c258d6372864cbf7812f2256f0749a1697c527b8
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-09-03 14:15:28 +0200

    Add Products table and insert sample data

---
+++ puzzles/puzzle#17.sql
@@ -0,0 +1,16 @@
+-- Puzzle #17
+
+-- Step 1 — Create table and insert sample data
+
+USE sql_learner;
+
+DROP TABLE IF EXISTS Products;
+CREATE TABLE Products (
+    Product VARCHAR(50),
+    Quantity INT
+);
+
+INSERT INTO Products (Product, Quantity) VALUES
+('Pencil', 3),
+('Eraser', 4),
+('Notebook', 2);
