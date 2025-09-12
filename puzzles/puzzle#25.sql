9245dfaa1193b5bf7b75c26ec062c5a3c836346e
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-09-12 11:55:51 +0200

    Compute total orders per vendor for each customer in puzzle#25

--- puzzles/puzzle#25.sql
+++ puzzles/puzzle#25.sql
@@ -18,3 +18,16 @@ INSERT INTO Orders25 VALUES
 (4, 2002, 'ACME', 7),
 (5, 2002, 'ACME', 16),
 (6, 2002, 'ACME', 5);
+
+
+-- Step 2 - sumujemy zamówienia per Customer + Vendor
+WITH VendorTotals AS (
+    SELECT 
+        CustomerID,
+        Vendor,
+        SUM(Cnt) AS TotalOrders
+    FROM Orders25
+    GROUP BY CustomerID, Vendor
+)
+SELECT * FROM VendorTotals;
+
