2da1d98231935fb6b1e071b690f00e37f1fd7963
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-09-24 12:38:40 +0200

    feat: create Sales38 table with input data

---
+++ puzzles/puzzle#38.sql
@@ -0,0 +1,21 @@
+-- Puzzle #38: Reporting Elements
+
+-- Step 1: Create table with input data
+DROP TABLE IF EXISTS Sales38;
+CREATE TABLE Sales38 (
+    Region VARCHAR(20),
+    Distributor VARCHAR(50),
+    Sales INT
+);
+
+INSERT INTO Sales38 (Region, Distributor, Sales) VALUES
+('North', 'ACE', 10),
+('South', 'ACE', 67),
+('East',  'ACE', 54),
+('North', 'ACME', 65),
+('South', 'ACME', 9),
+('East',  'ACME', 1),
+('West',  'ACME', 7),
+('North', 'Direct Parts', 8),
+('South', 'Direct Parts', 7),
+('West',  'Direct Parts', 12);
