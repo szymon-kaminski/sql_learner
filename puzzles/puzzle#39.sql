22cf71768b3b07cff8b741d1dc70e3252d5b309c
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-09-25 14:40:13 +0200

    feat: add SQL query to identify prime numbers from list

--- puzzles/puzzle#39.sql
+++ puzzles/puzzle#39.sql
@@ -13,3 +13,16 @@ INSERT INTO PrimeNumbers (IntegerValue) VALUES
 
 -- Preview input data
 SELECT * FROM PrimeNumbers ORDER BY IntegerValue;
+
+
+-- Step 2: Logika znajdowania liczb pierwszych
+SELECT p.IntegerValue
+FROM PrimeNumbers p
+WHERE p.IntegerValue > 1
+  AND NOT EXISTS (
+    SELECT 1
+    FROM PrimeNumbers d
+    WHERE d.IntegerValue > 1
+      AND d.IntegerValue < p.IntegerValue
+      AND p.IntegerValue % d.IntegerValue = 0
+  );
