7106d591afb02a864d642ed686990e8cbe780689
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-10-03 13:23:15 +0200

    feat: join schedules with activities

---
+++ puzzles/puzzle#47.sql
@@ -0,0 +1,28 @@
+-- Puzzle #47 - Work Schedule
+
+-- Step 1
+CREATE DATABASE IF NOT EXISTS sql_learner;
+USE sql_learner;
+
+CREATE TABLE Schedule (
+  ScheduleID CHAR(1),
+  StartTime DATETIME,
+  EndTime DATETIME
+);
+
+CREATE TABLE Activity (
+  ScheduleID CHAR(1),
+  Activity VARCHAR(20),
+  StartTime DATETIME,
+  EndTime DATETIME
+);
+
+INSERT INTO Schedule VALUES
+('A', '2021-10-01 10:00', '2021-10-01 15:00'),
+('B', '2021-10-01 10:15', '2021-10-01 12:15');
+
+INSERT INTO Activity VALUES
+('A', 'Meeting', '2021-10-01 10:00', '2021-10-01 10:30'),
+('A', 'Break',   '2021-10-01 12:00', '2021-10-01 12:30'),
+('A', 'Meeting', '2021-10-01 13:00', '2021-10-01 13:30'),
+('B', 'Break',   '2021-10-01 11:00', '2021-10-01 11:15');
