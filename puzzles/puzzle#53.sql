c0cd5574a2403d05e0a511e84e2a714b350efe52
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-10-13 13:33:54 +0200

    Create Spouses table and insert relationship data

--- puzzles/puzzle#53.sql
+++ puzzles/puzzle#53.sql
@@ -3,3 +3,19 @@
 -- Step 1 - Create Database and Use It
 CREATE DATABASE IF NOT EXISTS sql_learner;
 USE sql_learner;
+
+
+-- Step 2 - Create the Source Table
+
+CREATE TABLE Spouses (
+    PrimaryID VARCHAR(20),
+    SpouseID VARCHAR(20)
+);
+
+INSERT INTO Spouses (PrimaryID, SpouseID) VALUES
+('Pat', 'Charlie'),
+('Jordan', 'Casey'),
+('Ashley', 'Dee'),
+('Charlie', 'Pat'),
+('Casey', 'Jordan'),
+('Dee', 'Ashley');
