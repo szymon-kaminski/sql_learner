1484a11fc648ca9c06cc2bd00a62b2dc171a96ca
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-10-17 13:47:59 +0200

    Create anchor part of recursive CTE extracting first word

--- puzzles/puzzle#57.sql
+++ puzzles/puzzle#57.sql
@@ -15,3 +15,31 @@ CREATE TABLE Statements (
 INSERT INTO Statements VALUES
 (1, 'SELECT EmpID FROM Employees;'),
 (2, 'SELECT * FROM Transactions;');
+
+
+-- Step 2: Create base anchor part of recursive CTE
+
+WITH RECURSIVE SplitWords AS (
+    SELECT
+        1 AS RowNumber,
+        QuoteID,
+        StatementText AS String,
+        1 AS Starts,
+        CASE 
+            WHEN LOCATE(' ', StatementText) > 0 
+            THEN LOCATE(' ', StatementText) - 1
+            ELSE LENGTH(StatementText)
+        END AS Ends,
+        CASE 
+            WHEN LOCATE(' ', StatementText) > 0 
+            THEN SUBSTRING(StatementText, 1, LOCATE(' ', StatementText) - 1)
+            ELSE StatementText
+        END AS Word,
+        CASE 
+            WHEN LOCATE(' ', StatementText) > 0 
+            THEN SUBSTRING(StatementText, LOCATE(' ', StatementText) + 1)
+            ELSE ''
+        END AS Remainder
+    FROM Statements
+)
+SELECT * FROM SplitWords;
