b9725100820245386bdb3cc55261ed0126ef14e3
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-10-20 14:52:24 +0200

    feat: use recursive CTE to split and format expressions with spaces

--- puzzles/puzzle#58.sql
+++ puzzles/puzzle#58.sql
@@ -24,8 +24,41 @@ INSERT INTO Equations (Equation) VALUES
 SELECT * FROM Equations;
 
 
--- Step 2: Rozdzielenie liczb operatorów
+-- Step 2: Rozdzielenie liczb i operatorów
 SELECT
     Equation,
     REGEXP_REPLACE(Equation, '[+-]', ' ') AS Cleaned
 FROM Equations;
+
+
+-- Step 3: Rozdzielenie liczb i operatorów przy pomocy CTE
+WITH RECURSIVE SplitOperators AS (
+    SELECT
+        Equation,
+        1 AS pos,
+        SUBSTRING(Equation, 1, 1) AS char
+    FROM Equations
+
+    UNION ALL
+
+    SELECT
+        Equation,
+        pos + 1,
+        SUBSTRING(Equation, pos + 1, 1)
+    FROM SplitOperators
+    WHERE pos < LENGTH(Equation)
+),
+Parsed AS (
+    SELECT
+        Equation,
+        GROUP_CONCAT(
+            CASE
+                WHEN char IN ('+', '-') THEN CONCAT(' ', char, ' ')
+                ELSE char
+            END
+            ORDER BY pos SEPARATOR ''
+        ) AS Expression
+    FROM SplitOperators
+    GROUP BY Equation
+)
+SELECT * FROM Parsed;
