9924de9421982cb0cf611028e2dff506024c3621
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-10-28 13:42:09 +0100

    data: insert vehicle dataset for Puzzle #62

--- puzzles/puzzle#62.sql
+++ puzzles/puzzle#62.sql
@@ -10,3 +10,18 @@ CREATE TABLE Vehicles (
     Model VARCHAR(50),
     Price INT
 );
+
+
+-- Step 2: Insert sample data
+INSERT INTO Vehicles (VehicleID, Type, Model, Price) VALUES
+(1, 'Car', 'Rolls-Royce Phantom', 460000),
+(2, 'Car', 'Cadillac CT5', 39000),
+(3, 'Car', 'Porsche Boxster', 63000),
+(4, 'Car', 'Lamborghini Spyder', 290000),
+(5, 'Boat', 'Malibu', 210000),
+(6, 'Boat', 'ATX 22-S', 85000),
+(7, 'Boat', 'Sea Ray SLX', 520000),
+(8, 'Boat', 'Mastercraft', 25000);
+
+-- Preview data
+SELECT * FROM Vehicles;
