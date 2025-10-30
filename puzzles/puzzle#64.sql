33c0b084d658225fe347f263e293e5e66a589b39
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-10-30 14:04:59 +0100

    feat: insert sample string data for between-quotes evaluation

--- puzzles/puzzle#64.sql
+++ puzzles/puzzle#64.sql
@@ -9,3 +9,16 @@ CREATE TABLE Strings (
     ID INT,
     Str VARCHAR(50)
 );
+
+
+-- Step 2 – Wstawienie danych testowych
+INSERT INTO Strings (ID, Str) VALUES
+(1, '"12345678901234"'),
+(2, '1"2345678901234"'),
+(3, '123"45678"901234"'),
+(4, '123"45678901234"'),
+(5, '12345678901"234"'),
+(6, '12345678901234');
+
+-- Preview data
+SELECT * FROM Strings;
