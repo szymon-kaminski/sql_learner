dcb3050389c69bc18d0b22f372d487e3766d05c1
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-11-06 13:24:54 +0100

    feat: insert team performance data by year

--- puzzles/puzzle#68.sql
+++ puzzles/puzzle#68.sql
@@ -9,3 +9,18 @@ CREATE TABLE IF NOT EXISTS Scores (
     Year INT,
     Score INT
 );
+
+
+-- Step 2: Insert data
+INSERT INTO Scores (Team, Year, Score) VALUES
+('Cougars', 2015, 50),
+('Cougars', 2016, 45),
+('Cougars', 2017, 65),
+('Cougars', 2018, 92),
+('Bulldogs', 2015, 65),
+('Bulldogs', 2016, 60),
+('Bulldogs', 2017, 58),
+('Bulldogs', 2018, 12);
+
+-- Preview input data
+SELECT * FROM Scores;
