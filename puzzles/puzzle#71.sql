26dba67ca1c8a3663fe318dc2475b1f6f0475f2c
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-11-14 14:32:26 +0100

    feat: Insert sample employee, temp, and permanent data

--- puzzles/puzzle#71.sql
+++ puzzles/puzzle#71.sql
@@ -18,3 +18,23 @@ CREATE TABLE PermanentEmployees (
     EmployeeID INT PRIMARY KEY,
     Department VARCHAR(50)
 );
+
+
+-- Step 2 - Insert sample data
+INSERT INTO Employees VALUES
+(1001, 'John'),
+(2002, 'Eric'),
+(3003, 'Jennifer'),
+(4004, 'Bob'),
+(5005, 'Stuart'),
+(6006, 'Angie');
+
+INSERT INTO TemporaryEmployees VALUES
+(1001, 'Engineering'),
+(2002, 'Sales'),
+(3003, 'Marketing');
+
+INSERT INTO PermanentEmployees VALUES
+(4004, 'Marketing'),
+(5005, 'Accounting'),
+(6006, 'Accounting');
