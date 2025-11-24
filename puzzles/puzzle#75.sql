b6af3c0e689bea003a0c28e3a5ad1ef9e7b708f0
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-11-24 14:36:31 +0100

    feat: create boxes table with dimensions

--- puzzles/puzzle#75.sql
+++ puzzles/puzzle#75.sql
@@ -4,3 +4,12 @@
 DROP DATABASE IF EXISTS puzzle75;
 CREATE DATABASE puzzle75;
 USE puzzle75;
+
+
+-- Step 2: Create table
+CREATE TABLE boxes (
+    BoxID CHAR(1),
+    Length INT,
+    Width INT,
+    Height INT
+);
