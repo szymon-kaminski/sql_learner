a570d0bd4e212de2bc217adf453c27d7aa3fb707
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-11-26 14:09:08 +0100

    feat:normalize dimensions using least, middle, greatest ordering

--- puzzles/puzzle#75.sql
+++ puzzles/puzzle#75.sql
@@ -27,30 +27,17 @@ INSERT INTO boxes (BoxID, Length, Width, Height) VALUES
 SELECT * FROM boxes;
 
 
--- Step 4: Normalize dimensions (sort the 3 values)
+-- Step 4: Normalize dimensions using LEAST + GREATEST
 WITH normalized AS (
     SELECT
         BoxID,
-        -- sort dimensions by smallest → largest
         LEAST(Length, Width, Height) AS dim1,
-        Length + Width + Height
+        (Length + Width + Height)
             - LEAST(Length, Width, Height)
             - GREATEST(Length, Width, Height) AS dim2,
         GREATEST(Length, Width, Height) AS dim3
     FROM boxes
-),
-
-
--- Step 5: Assign grouping ID
-groups AS (
-    SELECT
-        BoxID,
-        DENSE_RANK() OVER (
-            ORDER BY dim1, dim2, dim3
-        ) AS grouping_id
-    FROM normalized
 )
-
 
 -- Step 6: Final result
 SELECT *
