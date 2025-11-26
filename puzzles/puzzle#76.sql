347ebd6e9d4e61bdbe1a0e703d087e7fac9558c7
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-11-26 14:25:14 +0100

    feat: create batch_start and batch_lines tables

--- puzzles/puzzle#76.sql
+++ puzzles/puzzle#76.sql
@@ -4,3 +4,15 @@
 DROP DATABASE IF EXISTS puzzle76;
 CREATE DATABASE puzzle76;
 USE puzzle76;
+
+-- STEP 2: Create tables
+CREATE TABLE batch_start (
+    Batch CHAR(1),
+    BatchStart INT
+);
+
+CREATE TABLE batch_lines (
+    Batch CHAR(1),
+    LineNum INT,
+    Syntax VARCHAR(200)
+);
