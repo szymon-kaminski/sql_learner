4a57dc7641d18ac2e0fb1a8ba1f0e5cbec0ead09
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-11-26 14:25:53 +0100

    feat: insert sample data into both tables

--- puzzles/puzzle#76.sql
+++ puzzles/puzzle#76.sql
@@ -16,3 +16,17 @@ CREATE TABLE batch_lines (
     LineNum INT,
     Syntax VARCHAR(200)
 );
+
+
+-- STEP 3: Insert data
+INSERT INTO batch_start (Batch, BatchStart) VALUES
+('A', 1),
+('A', 5);
+
+INSERT INTO batch_lines (Batch, LineNum, Syntax) VALUES
+('A', 1, 'SELECT *'),
+('A', 2, 'FROM Account;'),
+('A', 3, 'GO'),
+('A', 4, NULL),
+('A', 5, 'TRUNCATE TABLE Accounts;'),
+('A', 6, 'GO');
