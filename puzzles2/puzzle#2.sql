1c7e8fd815862ef18c3fd06978d816e011d000e9
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-12-01 14:14:21 +0100

    feat: Create permutations table with MaxNumber and Permutation columns

--- puzzles2/puzzle#2.sql
+++ puzzles2/puzzle#2.sql
@@ -7,3 +7,10 @@ USE puzzle2;
 
 -- Step 2: Drop old table
 DROP TABLE IF EXISTS permutations;
+
+
+-- Step 3: Create output table
+CREATE TABLE permutations (
+    MaxNumber INT,
+    Permutation VARCHAR(50)
+);
