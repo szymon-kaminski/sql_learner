f6323c3fcd698f7ad45d65a6e06ef29c554e8114
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-12-01 14:13:41 +0100

    feat: Drop permutations table if it already exists

--- puzzles2/puzzle#2.sql
+++ puzzles2/puzzle#2.sql
@@ -1,5 +1,9 @@
 -- Puzzle #2: All Permutations
 
---Step 1: Create database
+-- Step 1: Create database
 CREATE DATABASE IF NOT EXISTS puzzle2;
 USE puzzle2;
+
+
+-- Step 2: Drop old table
+DROP TABLE IF EXISTS permutations;
