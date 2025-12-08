a9cc0808b67a2722ea372536f738fefc9ed746ec
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-12-08 14:28:13 +0100

    feat: recursive CTE generating expressions with concatenation

--- puzzles2/puzzle#5.sql
+++ puzzles2/puzzle#5.sql
@@ -15,3 +15,35 @@ CREATE TABLE results (
     expression VARCHAR(100),
     total INT
 );
+
+-- STEP 3 - REKURENCYJNE GENEROWANIE WYRAŻEŃ
+WITH RECURSIVE cte AS (
+    -- start od pierwszej liczby
+    SELECT 
+        CAST(n AS CHAR(50)) AS expr,
+        CAST(n AS SIGNED) AS total,
+        n AS last_num,
+        1 AS pos
+    FROM numbers
+    WHERE n = 1
+
+    UNION ALL
+
+    SELECT
+        -- konkatenacja
+        CONCAT(expr, n.n),
+        CASE 
+            WHEN last_num >= 0 
+            THEN total - last_num + CAST(CONCAT(last_num, n.n) AS SIGNED)
+            ELSE total - last_num - CAST(CONCAT(ABS(last_num), n.n) AS SIGNED)
+        END,
+        CASE 
+            WHEN last_num >= 0 
+            THEN CAST(CONCAT(last_num, n.n) AS SIGNED)
+            ELSE -CAST(CONCAT(ABS(last_num), n.n) AS SIGNED)
+        END,
+        pos + 1
+    FROM cte
+    JOIN numbers n ON n.n = pos + 1
+
+   
