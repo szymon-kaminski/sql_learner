e138c4119f860bf4f6317f920dfd61e22fa6e5a1
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-12-08 14:25:06 +0100

    feat: create numbers table with input values

---
+++ puzzles2/puzzle#5.sql
@@ -0,0 +1,9 @@
+-- PUZZLE #5: Add the numbers up
+
+-- STEP 1 - TABELA Z DANYMI
+DROP TABLE IF EXISTS numbers;
+CREATE TABLE numbers (
+    n INT PRIMARY KEY
+);
+
+INSERT INTO numbers (n) VALUES (1), (2), (3);
