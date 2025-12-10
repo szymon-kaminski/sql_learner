78be030d84d33efdefea66b70e955d80e9499b7f
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-12-10 13:49:17 +0100

    feat: create params table and insert n value

--- puzzles2/puzzle#6.sql
+++ puzzles2/puzzle#6.sql
@@ -4,3 +4,16 @@
 DROP DATABASE IF EXISTS puzzle6;
 CREATE DATABASE puzzle6;
 USE puzzle6;
+
+
+-- STEP 1 — PARAMETER (length n)
+DROP TABLE IF EXISTS params;
+CREATE TABLE params (
+    n INT NOT NULL
+);
+
+-- insert desired length here
+INSERT INTO params (n) VALUES (3);
+
+-- set variable @n from params for convenient usage in CTEs
+SELECT n INTO @n FROM params LIMIT 1;
