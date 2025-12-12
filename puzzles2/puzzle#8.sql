61af0af44a35ad06aead9d85b32b204670417089
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-12-12 14:19:34 +0100

    feat: create people table and insert A1..A5 (adults) and C1..C5 (children)

--- puzzles2/puzzle#8.sql
+++ puzzles2/puzzle#8.sql
@@ -6,7 +6,30 @@
 --  * each vehicle must contain at least one adult
 -- Expected total rows: 7,200
 
--- STEP 0 — BAZA
+-- STEP 0 - BAZA
 DROP DATABASE IF EXISTS puzzle8;
 CREATE DATABASE puzzle8;
 USE puzzle8;
+
+
+-- STEP 1 - PEOPLE
+DROP TABLE IF EXISTS people;
+CREATE TABLE people (
+    id INT PRIMARY KEY,
+    name VARCHAR(10),
+    is_adult TINYINT(1)
+);
+
+-- insert 5 adults A1..A5 and 5 children C1..C5
+TRUNCATE TABLE people;
+INSERT INTO people (id, name, is_adult) VALUES
+(1, 'A1', 1),
+(2, 'A2', 1),
+(3, 'A3', 1),
+(4, 'A4', 1),
+(5, 'A5', 1),
+(6, 'C1', 0),
+(7, 'C2', 0),
+(8, 'C3', 0),
+(9, 'C4', 0),
+(10,'C5', 0);
