0215e13e11db9e4c197a5ad620c3a4abdd32c766
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-12-16 14:24:34 +0100

    feat: verify total count equals 7200

--- puzzles2/puzzle#8.sql
+++ puzzles2/puzzle#8.sql
@@ -75,3 +75,7 @@ JOIN people p5 ON p5.person_id NOT IN (a1.person_id, a2.person_id, a3.person_id,
 JOIN people p6 ON p6.person_id NOT IN (a1.person_id, a2.person_id, a3.person_id, a4.person_id, p1.person_id, p2.person_id, p3.person_id, p4.person_id, p5.person_id)
 WHERE
     a1.is_adult = TRUE;
+
+
+-- Step 4 - verify
+SELECT COUNT(*) AS total_assignments FROM assignments;
