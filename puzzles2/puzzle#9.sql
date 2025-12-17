f52c500d138e2d7c4c088a5a17b114383298438a
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-12-17 14:41:13 +0100

    feat: create input table with SQL statements

--- puzzles2/puzzle#9.sql
+++ puzzles2/puzzle#9.sql
@@ -4,3 +4,14 @@
 DROP DATABASE IF EXISTS puzzle9;
 CREATE DATABASE puzzle9;
 USE puzzle9;
+
+
+-- STEP 1 - INPUT TABLE
+CREATE TABLE statements (
+    id INT PRIMARY KEY,
+    stmt VARCHAR(200)
+);
+
+INSERT INTO statements VALUES
+(1, 'SELECT EmpID FROM Emps;'),
+(2, 'SELECT * FROM Trans;');
