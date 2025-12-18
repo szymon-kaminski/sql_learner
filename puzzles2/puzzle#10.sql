8ede6e890aa5d9003c5c2a3004ffed7a47e73b54
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-12-18 15:02:43 +0100

    feat: add seating chart input table

--- puzzles2/puzzle#10.sql
+++ puzzles2/puzzle#10.sql
@@ -5,3 +5,13 @@
 DROP DATABASE IF EXISTS puzzle10;
 CREATE DATABASE puzzle10;
 USE puzzle10;
+
+
+-- STEP 1 - INPUT TABLE
+CREATE TABLE seating_chart (
+    seat_number INT PRIMARY KEY
+);
+
+INSERT INTO seating_chart (seat_number) VALUES
+(7),(13),(14),(15),(27),(28),(29),(30),
+(31),(32),(33),(34),(35),(52),(53),(54);
