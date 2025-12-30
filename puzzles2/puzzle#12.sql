a8c125f27109ea94af6f53ace611324b722133d9
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-12-30 14:11:15 +0100

    feat(puzzle12): generate sequence using recursive CTE'

--- puzzles2/puzzle#12.sql
+++ puzzles2/puzzle#12.sql
@@ -15,3 +15,22 @@ CREATE TABLE numbers_sequence (
 -- STEP 2 - BASE CASE (START VALUE)
 INSERT INTO numbers_sequence (step_number, value)
 VALUES (1, 1);
+
+
+-- STEP 3 - RECURSIVE LOGIC (CORE)
+WITH RECURSIVE seq AS (
+    SELECT
+        1 AS step_number,
+        1 AS value
+    UNION ALL
+    SELECT
+        step_number + 1,
+        CASE
+            WHEN value * 2 < 100 THEN value * 2
+            ELSE value + 1
+        END
+    FROM seq
+    WHERE value < 100
+)
+INSERT INTO numbers_sequence
+SELECT * FROM seq;
