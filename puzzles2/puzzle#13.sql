105a95820a799353e31ac5c90a8773756eef76ce
Szymon Kaminski <szymon.kaminski46@gmail.com>
2026-01-14 14:24:10 +0100

    feat: create table for Pascal triangle values

--- puzzles2/puzzle#13.sql
+++ puzzles2/puzzle#13.sql
@@ -4,3 +4,12 @@
 DROP DATABASE IF EXISTS puzzle13;
 CREATE DATABASE puzzle13;
 USE puzzle13;
+
+
+-- STEP 1 - OUTPUT TABLE
+CREATE TABLE pascal_triangle (
+    row_number INT NOT NULL,
+    position INT NOT NULL,
+    value INT NOT NULL,
+    PRIMARY KEY (row_number, position)
+);
