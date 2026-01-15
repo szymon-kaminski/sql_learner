7a7d31691420afb070c406cac9a2d950a31f8156
Szymon Kaminski <szymon.kaminski46@gmail.com>
2026-01-15 14:31:13 +0100

    feat: create pascal triangle table

--- puzzles2/puzzle#13.sql
+++ puzzles2/puzzle#13.sql
@@ -4,3 +4,12 @@
 DROP DATABASE IF EXISTS puzzle13;
 CREATE DATABASE puzzle13;
 USE puzzle13;
+
+
+-- STEP 1 - OUTPUT TABLE
+CREATE TABLE pascal_triangle (
+    row_no INT NOT NULL,
+    col_no INT NOT NULL,
+    val INT NOT NULL,
+    PRIMARY KEY (row_no, col_no)
+);
