e78e751ada6cd224cdd3eead5b9d37a093c7aa71
Szymon Kaminski <szymon.kaminski46@gmail.com>
2026-01-28 14:22:29 +0100

    feat: create simulation table

--- puzzles2/puzzle#16.sql
+++ puzzles2/puzzle#16.sql
@@ -4,3 +4,13 @@
 DROP DATABASE IF EXISTS puzzle16;
 CREATE DATABASE puzzle16;
 USE puzzle16;
+
+
+-- STEP 1 - SIMULATION TABLE
+CREATE TABLE simulations (
+    sim_id INT PRIMARY KEY,
+    prize_door INT,
+    chosen_door INT,
+    switch_win BOOLEAN,
+    stay_win BOOLEAN
+);
