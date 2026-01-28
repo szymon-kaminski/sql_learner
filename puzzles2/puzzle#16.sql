4669160488c3a78f603342fd357122b6962f1624
Szymon Kaminski <szymon.kaminski46@gmail.com>
2026-01-28 14:23:14 +0100

    feat: generate 10k Monty Hall simulations using recursive CTE

--- puzzles2/puzzle#16.sql
+++ puzzles2/puzzle#16.sql
@@ -14,3 +14,28 @@ CREATE TABLE simulations (
     switch_win BOOLEAN,
     stay_win BOOLEAN
 );
+
+
+-- STEP 2 - GENERATE 10,000 SIMULATIONS
+INSERT INTO simulations
+WITH RECURSIVE seq AS (
+    SELECT 1 AS sim_id
+    UNION ALL
+    SELECT sim_id + 1
+    FROM seq
+    WHERE sim_id < 10000
+)
+SELECT
+    sim_id,
+
+    FLOOR(1 + RAND() * 3) AS prize_door,
+    FLOOR(1 + RAND() * 3) AS chosen_door,
+
+    -- switching wins if initial choice was wrong
+    CASE
+        WHEN FLOOR(1 + RAND() * 3) <> FLOOR(1 + RAND() * 3) THEN NULL
+        ELSE NULL
+    END AS switch_win,  -- placeholder (fixed below)
+
+    NULL AS stay_win
+FROM seq;
