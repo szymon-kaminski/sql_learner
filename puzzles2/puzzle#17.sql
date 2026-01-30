4d223ecadcb1f036fc8a6e757a668e6a47bba5cb
Szymon Kaminski <szymon.kaminski46@gmail.com>
2026-01-30 13:00:22 +0100

    feat: simulate dice game using recursive CTE

--- puzzles2/puzzle#17.sql
+++ puzzles2/puzzle#17.sql
@@ -16,3 +16,46 @@ CREATE TABLE throws (
     score INT,
     PRIMARY KEY (game_id, throw_no)
 );
+
+
+-- STEP 2 - SIMULATE 1000 GAMES
+INSERT INTO throws
+WITH RECURSIVE games AS (
+    SELECT 1 AS game_id
+    UNION ALL
+    SELECT game_id + 1 FROM games WHERE game_id < 1000
+),
+play AS (
+    -- first throw
+    SELECT
+        g.game_id,
+        1 AS throw_no,
+        FLOOR(1 + RAND() * 6) AS dice,
+        0 AS score
+    FROM games g
+
+    UNION ALL
+
+    SELECT
+        p.game_id,
+        p.throw_no + 1,
+        FLOOR(1 + RAND() * 6) AS dice,
+
+        GREATEST(
+            0,
+            p.score +
+            CASE
+                WHEN p.dice BETWEEN 1 AND 5 THEN p.dice
+                ELSE -p.dice
+            END
+        ) AS score
+
+    FROM play p
+    WHERE p.score < 100
+)
+SELECT
+    game_id,
+    throw_no,
+    dice,
+    score
+FROM play;
