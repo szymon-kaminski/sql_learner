f52910bc9199dfbf405e4cf5d9c86b5a7867004d
Szymon Kaminski <szymon.kaminski46@gmail.com>
2026-01-30 12:59:43 +0100

    feat: create dice throw log table

--- puzzles2/puzzle#17.sql
+++ puzzles2/puzzle#17.sql
@@ -6,3 +6,13 @@ SET cte_max_recursion_depth = 20000;
 DROP DATABASE IF EXISTS puzzle17;
 CREATE DATABASE puzzle17;
 USE puzzle17;
+
+
+-- STEP 1 - THROW LOG
+CREATE TABLE throws (
+    game_id INT,
+    throw_no INT,
+    dice INT,
+    score INT,
+    PRIMARY KEY (game_id, throw_no)
+);
